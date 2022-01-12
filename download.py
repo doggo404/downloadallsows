b663f436f482f0a1756dc7a2d62355aed1da6c95
Doggo <69554869+doggo404@users.noreply.github.com>
2022-01-12 14:27:47 +0000

    more efficient, fixed bugs again
    god why do i work on this its so dumb

--- download.py
+++ download.py
@@ -1,36 +1,35 @@
 import requests
-import re
 import json
+import re
 import html
 
-"""
-This was made as a joke, it works but you probably shouldnt use this as it is awfully innefficent.
-Get authkey and torrent_pass by copying a download link.
-Get cookie by going to https://bemaniso.ws/index.php , pressing 'ctrl + shift + i' and going to the network tab
-reload page and click 'index.php' on the top, scroll down to request headers and copy the string after session=
-"""
+
 authkey = ''
 cookie = 'session='
 torrent_pass = ''
 
 headers = {'cookie': cookie}
 count = 1
-while count < 28595:
+while count < 35000:
     count = count + 1
-    ajax_url = 'https://bemaniso.ws/ajax.php?action=torrent&id=' + str(count)
-    url = "https://bemaniso.ws/torrents.php?action=download&id=" + str(count) + "&authkey=" + authkey + "&torrent_pass=" + torrent_pass
-    r = requests.get(url, headers=headers)
-    error = re.search('	<title>Error (.+?) :: #bemaniso tracker</title>', r.text)
+    info_url = 'https://bemaniso.ws/ajax.php?action=torrent&id=' + str(count)
+    download_url = "https://bemaniso.ws/torrents.php?action=download&id=" + str(count) + "&authkey=" + authkey + "&torrent_pass=" + torrent_pass
+    torrent_status = requests.get(info_url, headers=headers)
+    torrent_info = json.loads(torrent_status.content)
+    status = torrent_info['status']
 
-    if not error:
-        getname = requests.get(ajax_url, headers=headers)
-        json_info = json.loads(getname.content)
-        tname = json_info['response']['group']['name']
-        tname = tname.replace('/', '')
-        tname = html.unescape(tname)
-        open(f"{tname}.torrent", 'wb').write(r.content)
-        print(f'{tname} downloaded.')
+    if status == 'success':
+        torrent_download = requests.get(download_url, headers=headers)
+        torrent_name =  torrent_info['response']['group']['name']
+        torrent_name = torrent_name.replace('/', '')
+        torrent_name = torrent_name.replace('*', '')
+        torrent_name = torrent_name.replace('+', '')
+        torrent_name = torrent_name.replace(':', '')
+        torrent_name = torrent_name.replace('>', '')
+        torrent_name = torrent_name.replace('<', '')
+        torrent_name = html.unescape(torrent_name)
+        open(f"{torrent_name}.torrent", 'wb').write(torrent_download.content)
+        print(f"\n{torrent_name} downloaded. \n")
 
     else:
-        error_message = error.group(1)
-        print(f'Page gave Error {error_message}, torrent does not exist.')
+        print(f"status: {status}, torrent {count} does not exist")
